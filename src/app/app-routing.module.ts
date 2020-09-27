866d3c6cf3961bd5844bf4b68ca3b77fead702ac
mheredia <marcos.heredia@globant.com>
2020-09-27 12:57:37 -0300

    se agregan los componentes de layout del home y hojas de estilo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+// Page Components
+import { HomeComponent } from './page/home/home.component';
+
+const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent
+},{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full',
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material
+import {
+  MatStepperModule, MatSnackBarModule,
+  MatButtonModule,
+} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatSnackBarModule,
+  MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
